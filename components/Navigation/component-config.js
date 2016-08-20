f8d2dd981fb3b7a748b5a25fcf3a9c8269fd0b68
Nadav <nadavglickman@gmail.com>
2016-08-20 16:53:37 +0300

    adding navbar and drawer

---
+++ components/Navigation/component-config.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {CookbookRoute} from '../Cookbook/cookbook-container'
+
+let initialRoute = CookbookRoute;
+
+class ComponentsConfig {
+    static get getInitialComponent() {
+        return initialRoute
+    }
+
+    static get getComponents() {
+        return [
+            initialRoute
+        ]
+    }
+}
+
+export default ComponentsConfig;
